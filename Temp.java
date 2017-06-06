f60d78206c2dfda4acf6c87a0736969b53f4e887
Gianna <gianna7wu@gmail.com>
2017-06-06 15:08:39 -0700

    added CSVReader method

--- Temp.java
+++ Temp.java
@@ -3,18 +3,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Date;
 import java.sql.Timestamp;
+import java.lang.Object;
+import java.lang.Enum;
+import java.util.concurrent.TimeUnit;
+import java.util.*;
 
 public class Temp {
 
+  public static long getLengthOfEvent (String s, String e) {
+    Timestamp start = Timestamp.valueOf(s);
+    Timestamp end = Timestamp.valueOf(e);
+    return end.getTime() - start.getTime();
+  }
 
+  public static ArrayList<String[]> getDataFromCSVFile(String file){
+    String csvFile = file;
+    String line = "";
+    String cvsSplitBy = ",";
+    ArrayList<String[]> array = new ArrayList<String[]>();
+    try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
-    public static void main(String[] args) {
-      String text = "2011-10-02 18:48:05.123";
-      String text2 = "2011-10-03 18:48:05.123";
-      Timestamp ts = Timestamp.valueOf(text);
-      Timestamp ts2 = Timestamp.valueOf(text2);
-      System.out.println(ts);
-      System.out.println(ts2);
-      System.out.println(ts2.getTime() - ts.getTime() + " "); 
+        while ((line = br.readLine()) != null) {
+
+            // use comma as separator
+            String[] event = line.split(cvsSplitBy);
+
+            array.add(event);
+
+        }
+
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+    System.out.println(array.toString());
+    return array;
+  }
+
+  public static void main(String[] args) {
+      getDataFromCSVFile("events.csv");
+    }
+
 }

7dad64cb54fa6bd6adae0802d55d204aca6a96ee
csalas11 <carlossalas@Carloss-MacBook-Pro.local>
2017-06-07 10:43:39 -0700

    Even Class
    Class that defines an event object

---
+++ Event.java
@@ -0,0 +1,19 @@
+
+
+
+/*
+ * Start Time/ End Time/ Length/ Name of ID/ other....
+ *
+ */
+public class Event {
+	
+	public Event(int id, int startTime,int endTime, int length){
+
+
+
+	}
+
+
+
+
+}

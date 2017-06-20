335e4b60753ab90df2b089ab0138d94b5d9f7489
csalas11 <carlossalas@Carloss-MacBook-Pro.local>
2017-06-20 14:49:09 -0700

    GitHub Error Fixed
    Weird GitHub error removed.

--- CSVReader.java
+++ CSVReader.java
@@ -70,22 +70,20 @@ public class CSVReader {
 			if (temp0.getEndTime().compareTo(temp1.getStartTime()) == 0) {
 				alanAlgebraTable.get(x)[5] = 1;
 				alanAlgebraTable.get(x)[6] = 0;
-<<<<<<< HEAD
 				Integer[] arr = {temp0.getEventID(), temp1.getEventID(), (int)(temp1.getEndTime().getTime())};
-=======
+
 				String[] arr = {temp0.getEventID() + "", temp1.getEventID() + "", temp0.getEndTime() + ""};
->>>>>>> origin/master
+
 				a.add(arr);
 
 			}
 			else if (temp1.getEndTime().compareTo(temp0.getStartTime()) == 0) {
 				alanAlgebraTable.get(x)[5] = 0;
 				alanAlgebraTable.get(x)[6] = 1;
-<<<<<<< HEAD
 				Integer[] arr = {temp1.getEventID(), temp0.getEventID(), (int)(temp0.getEndTime().getTime())};
-=======
+
 				String[] arr = {temp1.getEventID()  + "", temp0.getEventID()  + "", temp1.getEndTime() + ""};
->>>>>>> origin/master
+
 				a.add(arr);
 			}
 			else {
@@ -213,28 +211,28 @@ public class CSVReader {
 
     //creates a csv file of the event ID and event lengths. it also returns an
     // ArrayList<String> object that contains the event ID and the event lengths
-<<<<<<< HEAD
+
     public static ArrayList<Integer[]> computeLengths(ArrayList<Event> a) throws FileNotFoundException{
       PrintWriter pw = new PrintWriter(new File("test.csv"));
       StringBuilder sb = new StringBuilder();
       ArrayList<Integer[]> arrayOfLengths = new ArrayList<Integer[]>();
-=======
+
     public static ArrayList<String[]> computeLengths(ArrayList<Event> a) throws FileNotFoundException{
       PrintWriter pw = new PrintWriter(new File("test.csv"));
       StringBuilder sb = new StringBuilder();
       ArrayList<String[]> arrayOfLengths = new ArrayList<String[]>();
->>>>>>> origin/master
+
       for (int x = 0; x < a.size(); x++) {
           sb.append(a.get(x).getEventID());
           sb.append(',');
           String length = Long.toString(a.get(x).getLength());
           sb.append(length);
           sb.append('\n');
-<<<<<<< HEAD
+
           Integer[] arr = {a.get(x).getEventID(), a.get(x).getEventID(), a.get(x).getLength()};
-=======
+
           String[] arr = {a.get(x).getEventID() + "", a.get(x).getEventID() + "", length};
->>>>>>> origin/master
+
           arrayOfLengths.add(arr);
       }
       pw.write(sb.toString());
@@ -263,10 +261,10 @@ public class CSVReader {
 
 
   /*Comparator for sorting the list by Student Name*/
-<<<<<<< HEAD
+
  public static Comparator<Integer[]> comparator = new Comparator<Integer[]>() {
 
-=======
+
 	public static Comparator<String[]> compString = new Comparator<String[]>() {
 
  	public int compare(String[] s1, String[] s2) {
@@ -277,7 +275,7 @@ public class CSVReader {
 
  public static Comparator<Integer[]> comparator = new Comparator<Integer[]>() {
 
->>>>>>> origin/master
+
 	public int compare(Integer[] s1, Integer[] s2) {
 	   //ascending order
 	   return s1[2] - s2[2];
@@ -303,13 +301,12 @@ public class CSVReader {
 				String computation = args[1];
 				if (computation.equals("lengths")) {
 					System.out.println("Events and their lengths (unsorted): ");
-<<<<<<< HEAD
+
 					ArrayList<Integer[]> lengthArray = computeLengths(array);
 					toStringComputations(lengthArray);
-=======
 					ArrayList<String[]> lengthArray = computeLengths(array);
 					toStringComputations2(lengthArray);
->>>>>>> origin/master
+
 					System.out.println("Events and their lengths (sorted): ");
 					Collections.sort(lengthArray, compString);
 					toStringComputations2(lengthArray);
